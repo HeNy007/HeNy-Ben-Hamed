c353c55c12030e3bd02e768bf07f3778dd85e621
HeNy007 <59735422+HeNy007@users.noreply.github.com>
2020-08-10 01:12:04 +0100

    Create sports.php

---
+++ 007/sports.php
@@ -0,0 +1,94 @@
+{ 
+   "results":[ 
+   
+ 
+       { 
+         "objectId":"zZSLPs2PtY",
+         "cover":{ 
+            "__type":"File",
+            "name":"f31e6817d4705a32085f5772d26f1c46_cover.png",
+            "url":"https://raw.githubusercontent.com/ismail096/versionFinalApp/master/spot.png"
+         },
+         "typeStreaming":"4",
+         "name":"Sports",
+         "player":"exo",
+         "error":false,
+         "user":{ 
+            "__type":"Pointer",
+            "className":"_User",
+            "objectId":"IYd4eAH47v"
+         },
+         "url":"https://madiratv.com/1-CaptainShoot/1-Country/1-Channel/57-Sport/allch1.m3u",
+         "createdAt":"2020-02-10T18:00:13.842Z",
+         "updatedAt":"2020-02-10T18:01:19.779Z"
+      },
+   
+   { 
+         "objectId":"zZSLPs2PtY",
+         "cover":{ 
+            "__type":"File",
+            "name":"f31e6817d4705a32085f5772d26f1c46_cover.png",
+            "url":"https://raw.githubusercontent.com/ismail096/versionFinalApp/master/bein.jpg"
+         },
+         "typeStreaming":"4",
+         "name":"Bein Sports (HD)",
+         "player":"exo",
+         "error":false,
+         "user":{ 
+            "__type":"Pointer",
+            "className":"_User",
+            "objectId":"IYd4eAH47v"
+         },
+         "url":"https://madiratv.com/2-BBLTv/allch1.m3u",
+         "createdAt":"2020-02-10T18:00:13.842Z",
+         "updatedAt":"2020-02-10T18:01:19.779Z"
+      },
+      
+      
+         { 
+         "objectId":"zZSLPs2PtY",
+         "cover":{ 
+            "__type":"File",
+            "name":"f31e6817d4705a32085f5772d26f1c46_cover.png",
+            "url":"https://raw.githubusercontent.com/ismail096/versionFinalApp/master/bein.jpg"
+         },
+         "typeStreaming":"4",
+         "name":"Bein Sports (Med)",
+         "player":"exo",
+         "error":false,
+         "user":{ 
+            "__type":"Pointer",
+            "className":"_User",
+            "objectId":"IYd4eAH47v"
+         },
+         "url":"https://madiratv.com/2-BBLTv/allch2.m3u",
+         "createdAt":"2020-02-10T18:00:13.842Z",
+         "updatedAt":"2020-02-10T18:01:19.779Z"
+      },
+      
+      
+         { 
+         "objectId":"zZSLPs2PtY",
+         "cover":{ 
+            "__type":"File",
+            "name":"f31e6817d4705a32085f5772d26f1c46_cover.png",
+            "url":"https://raw.githubusercontent.com/ismail096/versionFinalApp/master/bein.jpg"
+         },
+         "typeStreaming":"4",
+         "name":"Bein Sports (Low)",
+         "player":"exo",
+         "error":false,
+         "user":{ 
+            "__type":"Pointer",
+            "className":"_User",
+            "objectId":"IYd4eAH47v"
+         },
+         "url":"https://madiratv.com/2-BBLTv/allch3.m3u",
+         "createdAt":"2020-02-10T18:00:13.842Z",
+         "updatedAt":"2020-02-10T18:01:19.779Z"
+      }
+      
+      
+      
+   ]
+}
