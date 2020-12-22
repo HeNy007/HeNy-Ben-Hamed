b7426c79bcc0bbfd97049800bc9a94f7c115977a
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2020-12-22 11:59:08 +0100

    Create applic.js

---
+++ today-matches/applic.js
@@ -0,0 +1,28 @@
+ {
+ "@context": "https://schema.org",
+ "@type": "NewsArticle",
+ "mainEntityOfPage": {
+ "@type": "WebPage",
+ "@id": ""
+ },
+ "headline": "" 
+ 
+ "image": [
+ ""
+ ],
+ "datePublished": "",
+ "dateModified": "<!--Can't find substitution for tag [post.lastUpdatedISO8601]-->",
+ "author": {
+ "@type": "HeNy007",
+ "name": "Aliwix Team"
+ },
+ "publisher": {
+ "@type": "Organization",
+ "name": "أليويكس للبث المباشر",
+ "logo": {
+ "@type": "ImageObject",
+ "url": ""
+ }
+ },
+ "description": "Aliwix TV Apk" 
+ }
